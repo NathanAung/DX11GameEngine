b97f7644acc1593669561c6861f3ace5a67a0de4
Zayar Aung <z.zayar99@gmail.com>
2026-09-10 23:32:02 +0900

    Revert "Asset registry in data.pak, VFS-safe scene loading"
    This reverts commit 168c36bdf6a3051f833810324e90de81486653a8.

--- include/Engine/AssetManager.h
+++ include/Engine/AssetManager.h
@@ -17,7 +17,6 @@ namespace Engine
         Shader,
         ModelFile,
 		LuaScript,
-        Scene
     };
 
 	// metadata is used for serialization and deserialization of the asset registry, as well as for runtime asset management.
